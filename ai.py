2a6ca27b012f12821daf728f64fc38b83159b82d
Jeffrey Hu <zhiwehu@gmail.com>
2021-01-23 14:29:02 +0800

    update ai.py

--- ai.py
+++ ai.py
@@ -6,6 +6,43 @@ import cv2
 from time import sleep
 from picamera import PiCamera
 
+# 导入人脸模型数据
+face_cascade = cv2.CascadeClassifier("cv2models/haarcascade_frontalface_default.xml")
+
+# 检测人脸的自定义函数
+def __detect_face(frame):
+    # 识别人脸并保存到一个列表中
+    faces = face_cascade.detectMultiScale(frame, 1.3, 5)
+    # 列表中的每个人脸数据都是由4个值组成的：
+    # x,y表示左上角的位置，w,h表示宽度和高度，对每张人脸，我们画一个蓝色方框来框起来
+    if len(faces) > 0:
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (0, 0, 255), 2)
+    return frame
+
+def face():
+    # 打开摄像头
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        # 从摄像头读取数据
+        ret, frame = cap.read()
+        if ret:
+            # 检测人脸
+            __detect_face(frame)
+            # 显示视频数据
+            cv2.imshow("video", frame)
+            # 让这一帧停留1毫秒，这时如果我们按下q键，就跳出这个循环停止播放
+            # 如果不写cv2.waitKey()我们将看不到视频图像
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                break
+
+    # 关闭摄像头
+    cap.release()
+    # 关闭播放器
+    cv2.destroyAllWindows()
+
 def camera(filename="output.jpg"):
     camera = PiCamera()
     camera.resolution = (1024, 768)
@@ -14,13 +51,13 @@ def camera(filename="output.jpg"):
     camera.close()
 
 # 以下换成自己的百度云语音API KEY
-APP_ID = '9670645'
-API_KEY = 'qg4haN8b2bGvFtCbBGqhrmZy'
-SECRET_KEY = '585d4eccb50d306c401d7df138bb02e7'
+APP_ID = '23549160'
+API_KEY = 'MeyxezlB82jwihFtXT7Kdt7i'
+SECRET_KEY = 'BVeGYZ5DyP88IvquiNKm6kkW07m2Dhi8'
 client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
 
 # 语音识别
-def asr(f):
+def asr(f="output.wav"):
     wav = wave.open(f, 'rb')
     res = client.asr(wav.readframes(wav.getnframes()), 'pcm', 16000, {
         'dev_pid': 1936,
